b55a4dd5e8ab45c56ea85de9bac3b8a5a5e84db5
akhoz <adrian25092005@gmail.com>
2024-05-05 15:43:48 -0600

    Implementing Log In View

--- Frontend/src/views/LogIn.jsx
+++ Frontend/src/views/LogIn.jsx
@@ -1,8 +1,58 @@
-import { Button } from "flowbite-react";
+import { Link } from 'react-router-dom';
+import { FaArrowLeft } from "react-icons/fa";
+
 function LogIn () {
     return (
-        <div>
-            <Button>Click me</Button>
+        <div className="flex flex-col lg:h-screen lg:flex-row">
+            <div className="flex flex-col items-center justify-center mt-10 lg:mt-0 lg:w-3/4">
+                <img
+                    src="/img/iconic-caps-logo.png"
+                    alt="iconic-caps-logo"
+                    className="w-1/6"/>
+                <h1 className="text-4xl font-bold">Log In</h1>
+                <div className="flex flex-col w-1/3 mt-5">
+                    <input
+                        type="text"
+                        id="text"
+                        className="border-0 border-b-2 border-black p-1 my-5"
+                        placeholder="Username"
+                    />
+                    <input
+                        type="password"
+                        id="password"
+                        className="border-0 border-b-2 border-black p-1"
+                        placeholder="Password"
+                    />
+                    <div className="flex flex-row items-center my-5">
+                        <input
+                            type="checkbox"
+                            id="checkbox"
+                            className="border-1 border-black"
+                        />
+                        <p className="text-md text-gray-600 pl-3">
+                            Remember Me
+                        </p>
+                    </div>
+                    <button className="bg-black text-white font-bold p-2 rounded-md mb-5">
+                        Log In
+                    </button>
+                    <a
+                        className="text-md text-gray-600 mb-2"
+                        href="#">
+                        I forgot my password
+                    </a>
+                    <Link to="./SignUp" className="text-md text-gray-600">
+                        Create an Account
+                    </Link>
+                </div>
+            </div>
+            <div className="hidden lg:block flex-col bg-black justify-center pt-28 w-1/4">
+                <img src="/img/slogan.png" alt="slogan"/>
+            </div>
+            <Link to="/" className="flex flex-row items-center space-x-2 absolute top-0 left-0 ml-4 mt-4 text-black font-bold text-md">
+                <FaArrowLeft/>
+                <span>Home</span>
+            </Link>
         </div>
     )
 }
