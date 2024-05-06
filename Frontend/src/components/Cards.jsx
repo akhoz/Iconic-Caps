c50c761970474bb02915082f80f9632400cab795
akhoz <adrian25092005@gmail.com>
2024-05-05 22:13:39 -0600

    pl

--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 function DeveloperCard({name, imageUrl, description }) {
-  return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <img className="w-full" src={imageUrl} alt={name}/>
-        <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">{name}</div>
-            <p className="text-gray-700 text-base">{description}</p>
+    return (
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img className="w-full" src={imageUrl} alt={name}/>
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{name}</div>
+                <p className="text-gray-700 text-base">{description}</p>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default DeveloperCard;
