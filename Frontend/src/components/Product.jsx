4f8f6908056f802142cd3c15d68b99839afcc84b
akhoz <adrian25092005@gmail.com>
2024-05-08 09:38:45 -0600

    Fixing Shop View

--- Frontend/src/components/Product.jsx
+++ Frontend/src/components/Product.jsx
@@ -4,26 +4,26 @@ import BagButton from "./BagButton.jsx";
 function Product(props) {
     return (
         <div className="flex flex-col transition-transform transform hover:scale-105">
-            <div className="bg-gray-300 rounded-xl mb-2">
+            <div className="bg-gray-100 mb-2 p-5">
                 <img
                     src={props.imgSrc}
                     alt={props.imgAlt}
-                    className="w-40 h-40 object-cover p-1"/>
+                    className="w-40 h-40 object-cover p-1 md:w-60 md:h-60 lg:w-80 lg:h-80"/>
             </div>
-            <h1 className="font-bold text-xl">
+            <h1 className="font-bold text-xl lg:text-2xl">
                 {props.model}
             </h1>
-            <p className="text-black">
+            <p className="text-black lg:text-lg">
                 {props.brand}
             </p>
-            <p className="text-gray-600">
+            <p className="text-gray-600 lg:text-lg">
                 {props.category}
             </p>
-            <p className="text-gray-600">
+            <p className="text-gray-600 lg:text-lg">
                 {`Available stock: ${props.stock}`}
             </p>
             <hr className="border-1 border-gray-300 my-1 w-full"/>
-            <p className="font-bold">
+            <p className="font-bold lg:text-lg">
                 {`$${props.price}`}
             </p>
         </div>
