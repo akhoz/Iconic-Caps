bf76e021785ff92b27458613b3cde7b2a76646c2
akhoz <adrian25092005@gmail.com>
2024-05-10 13:21:12 -0600

    Updating Checkout

--- Frontend/src/components/CheckoutProduct.jsx
+++ Frontend/src/components/CheckoutProduct.jsx
@@ -1,26 +1,29 @@
 
 function CheckoutProduct() {
     return (
-        <div className="flex flex-row justify-between items-center">
-            <div>
-                <p>
-                    produc
-                </p>
-                <p>
-                    Descrip
-                </p>
+        <div className="flex w-10/12 flex-col" data-aos="fade-up">
+            <div className="flex w-full flex-row justify-between items-center">
+                <div className="flex flex-row w-1/2 lg:py-10 lg:space-x-10 xl:space-x-20">
+                    <p>
+                        produc
+                    </p>
+                    <p>
+                        Descrip
+                    </p>
+                </div>
+                <div className="flex flex-row justify-between items-center w-1/2 lg:py-10">
+                    <p>
+                        $100
+                    </p>
+                    <p>
+                        3
+                    </p>
+                    <p>
+                        $100
+                    </p>
+                </div>
             </div>
-            <div>
-                <p>
-                    $100
-                </p>
-                <p>
-                    Quantity: 1
-                </p>
-                <p>
-                    Total: $100
-                </p>
-            </div>
+            <hr className="w-full border-b border-gray-100 xl:mt-10 xl:mb-14"/>
         </div>
     )
 }
--- Frontend/src/views/Checkout.jsx
+++ Frontend/src/views/Checkout.jsx
@@ -1,12 +1,44 @@
 import CheckoutProduct from "../components/CheckoutProduct.jsx";
+import {Link} from "react-router-dom";
+import {FaArrowLeft} from "react-icons/fa";
 
 function Checkout() {
     return (
         <div className="flex flex-col items-center justify-center my-24">
-            <h1 className="text-black font-bold text-3xl">
+            <h1 className="text-black font-bold text-3xl" data-aos="fade-up">
                 Your Bag
             </h1>
+            <div className="flex w-10/12 flex-row justify-between items-center mt-20" data-aos="fade-up">
+                <p className="w-1/2 text-lg font-bold">
+                    Product
+                </p>
+                <div className="flex flex-row justify-between items-center w-1/2 text-lg font-bold">
+                    <p>
+                        Price
+                    </p>
+                    <p>
+                        Quantity
+                    </p>
+                    <p>
+                        Total
+                    </p>
+                </div>
+            </div>
+            <hr className="w-10/12 border-b border-gray-100 xl:mb-8" data-aos="fade-up"/>
             <CheckoutProduct/>
+            <CheckoutProduct/>
+            <div className="flex flex-row justify-between w-10/12" data-aos="fade-up">
+                <p className="flex flex-col justify-start items-start w-1/2">
+                    Payment Form
+                </p>
+                <p className="flex flex-col justify-start items-end w-1/2">
+                    Information
+                </p>
+            </div>
+            <Link to="/Shop" className="flex flex-row items-center space-x-2 absolute top-0 left-0 ml-4 mt-4 text-black font-bold text-md transition-transform transform hover:scale-105" data-aos="fade-up">
+                <FaArrowLeft/>
+                <span>Keep Shopping</span>
+            </Link>
         </div>
     );
 }
