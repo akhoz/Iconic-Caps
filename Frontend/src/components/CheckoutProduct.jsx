258ad34cca90240a1c6c753d84ba0facbe4d75a0
akhoz <adrian25092005@gmail.com>
2024-05-10 12:04:31 -0600

    Checkout First Approach

---
+++ Frontend/src/components/CheckoutProduct.jsx
@@ -0,0 +1,28 @@
+
+function CheckoutProduct() {
+    return (
+        <div className="flex flex-row justify-between items-center">
+            <div>
+                <p>
+                    produc
+                </p>
+                <p>
+                    Descrip
+                </p>
+            </div>
+            <div>
+                <p>
+                    $100
+                </p>
+                <p>
+                    Quantity: 1
+                </p>
+                <p>
+                    Total: $100
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default CheckoutProduct
--- Frontend/src/views/Checkout.jsx
+++ Frontend/src/views/Checkout.jsx
@@ -1,8 +1,12 @@
+import CheckoutProduct from "../components/CheckoutProduct.jsx";
 
 function Checkout() {
     return (
-        <div>
-            <h1>Checkout</h1>
+        <div className="flex flex-col items-center justify-center my-24">
+            <h1 className="text-black font-bold text-3xl">
+                Your Bag
+            </h1>
+            <CheckoutProduct/>
         </div>
     );
 }
