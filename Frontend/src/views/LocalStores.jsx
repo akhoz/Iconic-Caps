455d42f104ac988213101fa73539e71858a9ec1b
akhoz <adrian25092005@gmail.com>
2024-05-15 08:25:05 -0600

    Updating Stores View

--- Frontend/src/views/LocalStores.jsx
+++ Frontend/src/views/LocalStores.jsx
@@ -1,6 +1,34 @@
 import Stores from '../components/Stores.jsx';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function LocalStores() {
+    const employeesUri = "http://localhost:8000/empleados";
+    const storesUri = "http://localhost:8000/sucursales/";
+
+    const [employees, setEmployees] = useState([]);
+    useEffect(() => {
+        getEmployees();
+    });
+
+    const [stores, setStores] = useState([]);
+    useEffect(() => {
+        getStores();
+    });
+
+    const getEmployees = async () => {
+        const res = await axios.get(employeesUri);
+        setEmployees(res.data);
+    }
+
+    const getStores = async () => {
+        const res = await axios.get(storesUri);
+        setStores(res.data);
+    }
+
+    console.log(stores)
+    console.log(employees)
+
     return (
         <div className="w-full">
             <h1 className="text-3xl font-bold text-center mb-5">Our Stores</h1>
