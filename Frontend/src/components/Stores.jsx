671950a88fab724b4bcea6003fc9131365e6643b
akhoz <adrian25092005@gmail.com>
2024-05-15 11:04:34 -0600

    Improving Local Store Design

--- Frontend/src/components/Stores.jsx
+++ Frontend/src/components/Stores.jsx
@@ -1,31 +1,33 @@
 import PropTypes from 'prop-types';
-import {useState} from "react";
 
 function Stores (props) {
-const storeIndex = props.storeIndex;
+    const storeIndex = props.storeIndex;
 
     return (
-        <div className={`flex w-full ${storeIndex % 2 === 0  ? 'bg-white flex-row-reverse text-black text-right' : 'flex-row bg-black text-white text-left'}`}>
+        <div className={`flex w-full ${storeIndex % 2 === 0 ? 'flex-row-reverse text-right bg-white text-black' : 'flex-row text-left bg-black text-white relative'}`}>
             <div className="w-2/5 p-5">
-                <img src={props.imgSrc} alt={props.imgSrc} className="w-full h-full object-cover rounded-lg" />
+                <img src={props.imgSrc} alt={props.imgSrc} className="w-full h-full object-cover rounded-lg"/>
             </div>
-            <div className="w-3/5 px-5 py-8 flex flex-col justify-between">
-                <div className="mb-5">
+            <div
+                className={`w-1/3 px-5 py-8 flex ${storeIndex % 2 === 0 ? 'flex-row-reverse' : 'flex-row'} justify-between w-full`}>
+                <div className="flex flex-col mb-5 w-1/2">
                     <h2 className="text-xl font-semibold">
                         {props.name}
                     </h2>
-                    <p className={`${storeIndex % 2 === 0 ? 'ml-60' : 'mr-60'}`}>
+                    <p className={`${storeIndex % 2 === 0 ? 'ml-20' : 'mr-20'} text-justify`}>
                         {props.description}
                     </p>
                 </div>
-                <div>
+                <div className="w-1/3">
                     <h3 className="text-xl font-semibold">Employees</h3>
                     <div className='flex flex-col'>
                         {props.employees.map((employee, index) => (
                             <p key={index}>{employee}</p>
                         ))}
                     </div>
-                    <div className="mt-5">
+                </div>
+                <div className="w-1/3">
+                    <div>
                         <h3 className="text-xl font-semibold">Contact Us</h3>
                         <p>
                             <ul>
@@ -33,15 +35,18 @@ const storeIndex = props.storeIndex;
                             </ul>
                         </p>
                     </div>
-                    <div>
-                        <a href={props.location} target="_blank" rel="noopener noreferrer">
-                            <button className={` ${storeIndex % 2 === 0 ? 'bg-black text-white' : 'bg-white text-black'} font-bold py-2 px-4 rounded mb-1 mt-1 duration-300 transition-transform transform hover:scale-110`}>
-                                See in the Map
-                            </button>
-                        </a>
-                    </div>
                 </div>
             </div>
+            <a
+                href={props.location}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`absolute ${storeIndex % 2 === 0 ? 'left-5' : 'bottom-5 right-5'}`}>
+                <button
+                    className={` ${storeIndex % 2 === 0 ? 'bg-black text-white' : 'bg-white text-black'} font-bold py-2 px-4 rounded mb-1 mt-1 duration-300 transition-transform transform hover:scale-110`}>
+                    See in the Map
+                </button>
+            </a>
         </div>
     );
 }
