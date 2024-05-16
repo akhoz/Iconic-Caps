12f8c01e10c339282e80c97de810e52474d54266
akhoz <adrian25092005@gmail.com>
2024-05-16 14:16:06 -0600

    Implementing Database Indexes and Users

---
+++ Database/users.sql
@@ -0,0 +1,17 @@
+use iconiccaps;
+
+-- Super usuario
+CREATE USER super_usuario@localhost IDENTIFIED BY 'super';
+GRANT ALL PRIVILEGES ON iconiccaps.* TO super_usuario@localhost;
+
+-- Usuario normal (tablas y funciones)
+CREATE USER usuario_normal@localhost IDENTIFIED BY 'normal';
+GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE ON iconiccaps.* TO usuario_normal@localhost;
+
+-- Usuaio de respaldo
+CREATE USER usuario_respaldo@localhost IDENTIFIED BY 'respaldo';
+GRANT SELECT ON iconiccaps.* TO usuario_respaldo@localhost; -- Falta darle mas permisos
+
+SHOW GRANTS FOR super_usuario@localhost;
+SHOW GRANTS FOR usuario_normal@localhost;
+SHOW GRANTS FOR usuario_respaldo@localhost;
