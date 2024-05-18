7c7712dee803d1cfa74b6604b9ebfee926ced07c
RamchelOrtiz <ram.ortizb@gmail.com>
2024-05-18 09:49:36 -0600

    Update Comentario COntroller

--- Backend/routes/Comentarioroutes.js
+++ Backend/routes/Comentarioroutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getAllComentarios, getComentarioByModel, getComentarioByCedula, createComentario, 
+import { getAllComentarios, getComentarioByModel, getComentarioByCedula, getCometarioById, createComentario, 
     updateComentario, deleteComentario
 } from '../controllers/ComentarioController.js'
 
@@ -8,6 +8,7 @@ const router = express.Router()
 router.get('/', getAllComentarios)
 router.get('/modelo/:ModeloProducto', getComentarioByModel)
 router.get('/cedula/:CedulaCliente', getComentarioByCedula)
+router.get('/id/:IdComentario', getCometarioById)
 router.post('/', createComentario)
 router.put('/:id', updateComentario)
 router.delete('/:id', deleteComentario)
