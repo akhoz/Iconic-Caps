19e5c8e4dc0af6836419d8ed28bdd723fd00a03a
akhoz <adrian25092005@gmail.com>
2024-05-20 09:33:43 -0600

    Fixing performance issue

--- Frontend/src/contexts/UserContext.jsx
+++ Frontend/src/contexts/UserContext.jsx
@@ -1,32 +1,32 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useCallback } from "react";
 import PropTypes from 'prop-types';
-import {useCookies} from "react-cookie";
-import axios from 'axios'
+import { useCookies } from "react-cookie";
+import axios from 'axios';
 
 const UserContext = createContext();
 
-export const UserProvider = ({children}) => {
+export const UserProvider = ({ children }) => {
     const [cookie, setCookie, removeCookie] = useCookies(['username']);
     const [user, setUser] = useState(null);
 
-    const logIn = (userData) => {
+    const logIn = useCallback((userData) => {
         setUser(userData);
         sessionStorage.setItem('user', JSON.stringify(userData));
-    }
+    }, []);
 
-    const logOut = () => {
+    const logOut = useCallback(() => {
         setUser(null);
         removeCookie('username', { path: '/' });
         sessionStorage.removeItem('user');
-    }
+    }, [removeCookie]);
 
-    const checkCookies = async () => {
+    const checkCookies = useCallback(async () => {
         if (cookie.username) {
             const res = await axios.get(`http://localhost:8000/clientes/${cookie.username}`);
             const clienteData = res.data;
             logIn(clienteData);
         }
-    }
+    }, [cookie.username, logIn]);
 
     return (
         <UserContext.Provider value={{ user, logIn, logOut, checkCookies }}>
