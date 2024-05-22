87f73cc9ca5154621fca22521e5bf7a3a097d1d8
Hector <hcaravacavargas@gmail.com>
2024-05-22 15:29:06 -0600

    Changing description from in our team page

--- Frontend/src/components/DeveloperCard.jsx
+++ Frontend/src/components/DeveloperCard.jsx
@@ -7,11 +7,12 @@ function DeveloperCard(props) {
   return (
     <div className="max-w-xs rounded overflow-hidden shadow-lg transition-transform transform hover:scale-105">
         <img className="w-full" src={props.image} alt={props.name}/>
-        <div className="px-6 py-10">
+        <div className="px-6 py-8">
             <div className="font-bold text-xl mb-2">{props.name}</div>
             <p className="text-black text-base mb-2">{props.description}</p>
         </div>
-        <div className="flex justify-center space-x-4 py-2">
+        <h1 className="flex justify-center text-xl">More information:</h1>
+        <div className="flex justify-center mb-4 mt-4 ml-4 mr-4">
             <a href={props.ig} target="_blank" rel="noopener noreferrer"><FaInstagram className="text-black text-4xl"/></a>
             <a href={props.x} target="_blank" rel="noopener noreferrer"><FaXTwitter className="text-black text-4xl"/></a>
             <a href={props.github} target="_blank" rel="noopener noreferrer"><FaGithub className="text-black text-4xl"/></a>
