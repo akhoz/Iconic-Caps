287acb5b977fd42b6075c864fe2222c587867ddd
RamchelOrtiz <ram.ortizb@gmail.com>
2024-05-22 09:00:46 -0600

    Update backend

--- Backend/controllers/RepartidorController.js
+++ Backend/controllers/RepartidorController.js
@@ -1,7 +1,7 @@
 
 // importamos el modelo
 import RepartidorModel from "../models/RepartidorModel.js"
-
+import PersonaModel from "../models/PersonaModel.js"
 //** Métodos para el CRUD**/
 
     //Mostrar todos los registros
@@ -12,6 +12,50 @@ export const getAllRepartidores = async (req, res) => {
 
     } catch (error) {
         res.json({message: error.message})
+    }
+};
+
+// Mostrar un registro
+export const getRepartidor = async (req, res) => {
+    try {
+        const repartidor = await RepartidorModel.findOne({
+            where: {
+                CedulaRepartidor: req.params.Cedula
+            }, 
+            include: [{model: PersonaModel}]
+        });
+        res.json(repartidor);
+    } catch (error) {
+        res.json({ message: error.message });
+    }
+};
+
+// Crear un registro
+export const createRepartidor = async (req, res) => {
+    try {
+        await RepartidorModel.create({
+            CedulaRepartidor: req.body.CedulaRepartidor,
+            NumeroSucursalAsignada: req.body.NumeroSucursalAsignada
+        });
+        res.json("Repartidor creado");
+    } catch (error) {
+        res.json({ message: error.message });
+    }
+};
+
+// Actualizar un registro
+export const updateRepartidor = async (req, res) => {
+    try {
+        await RepartidorModel.update({
+            NumeroSucursalAsignada: req.body.NumeroSucursalAsignada
+        }, {
+            where: {
+                CedulaRepartidor: req.params.Cedula
+            }
+        });
+        res.json("Repartidor actualizado");
+    } catch (error) {
+        res.json({ message: error.message });
     }
 };
 
