8785d50c190117ca46de091315049f198052f88e
akhoz <adrian25092005@gmail.com>
2024-05-22 08:19:55 -0600

    Implementing Delete Store Modal

--- Frontend/src/components/admin/DeleteStoreModal.jsx
+++ Frontend/src/components/admin/DeleteStoreModal.jsx
@@ -4,51 +4,51 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { GiBilledCap } from "react-icons/gi";
 
-function DeleteProductModal(props) {
-    const [modelo, setModelo] = useState('');
-    const [producto, setProducto] = useState(null);
-    const [invalidProduct, setInvalidProduct] = useState(false);
+function DeleteStoreModal(props) {
+    const [numeroSucursal, setNumeroSucursal] = useState('');
+    const [sucursal, setSucursal] = useState(null);
+    const [invalidSucursal, setInvalidSucursal] = useState(false);
     const [URI, setURI] = useState('');
 
     const handleModelChange = (event) => {
-        setModelo(event.target.value);
+        setNumeroSucursal(event.target.value);
     };
 
     useEffect(() => {
-        if (modelo) {
-            setURI(`http://localhost:8000/productos/${modelo}`);
+        if (numeroSucursal) {
+            setURI(`http://localhost:8000/sucursales/${numeroSucursal}`);
         } else {
-            setProducto(null);
-            setInvalidProduct(false);
+            setSucursal(null);
+            setInvalidSucursal(false);
         }
-    }, [modelo]);
+    }, [numeroSucursal]);
 
     useEffect(() => {
         if (URI) {
-            getProductos();
+            getSucursal();
         }
     }, [URI]);
 
     useEffect(() => {
-        if (producto === null || producto.length === 0) {
-            setInvalidProduct(true);
+        if (sucursal === null || sucursal.length === 0) {
+            setInvalidSucursal(true);
         } else {
-            setInvalidProduct(false);
+            setInvalidSucursal(false);
         }
-    }, [producto]);
+    }, [sucursal]);
 
-    const getProductos = async () => {
+    const getSucursal = async () => {
         try {
             const res = await axios.get(URI);
-            setProducto(res.data);
+            setSucursal(res.data[0]);
         } catch (error) {
-            setProducto(null);
-            setInvalidProduct(true);
+            setSucursal(null);
+            setInvalidSucursal(true);
         }
     };
 
-    const handleDeleteProduct = async () => {
-        if (!invalidProduct) {
+    const handleDeleteSucursal = async () => {
+        if (!invalidSucursal) {
             await fetch(URI, {
                 method: 'DELETE',
             });
@@ -57,32 +57,34 @@ function DeleteProductModal(props) {
         }
     }
 
+    console.log(sucursal)
+    console.log(URI)
     return (
-        <div className="flex flex-row relative rounded-lg overflow-hidden w-2/3 " data-aos="zoom-in">
+        <div className="flex flex-row relative rounded-lg overflow-hidden w-4/5 " data-aos="zoom-in">
             <div className="flex flex-col items-center justify-center bg-white w-1/2 py-20 overflow-hidden">
                 <h1 className="text-2xl font-bold">
-                    Delete Product
+                    Delete Store
                 </h1>
                 <p className="text-lg mt-3">
-                    Write the product model below
+                    Write the store number below
                 </p>
                 <input
-                    type="text"
-                    id="model"
-                    className="border-0 border-b-2 p-1 mt-8 focus:border-b-2 focus:border-black focus:ring-0 focus:outline-0"
-                    placeholder="Product Model"
+                    type="number"
+                    id="number"
+                    className="border-0 border-b-2 p-1 mt-8 focus:border-b-2 focus:border-black focus:ring-0 focus:outline-0 w-1/2 remove-arrow"
+                    placeholder="Store Number"
                     onChange={handleModelChange}
                 />
                 <button className={`mt-8 rounded-lg w-1/2 py-3 duration-500 bg-black text-white
-                    ${invalidProduct ? 'hover:bg-red-500' : 'hover:bg-white hover:text-black hover:border hover:border-black'}`}
-                    onClick={handleDeleteProduct}>
-                    Delete Product
+                    ${invalidSucursal ? 'hover:bg-red-500' : 'hover:bg-white hover:text-black hover:border hover:border-black'}`}
+                        onClick={handleDeleteSucursal}>
+                    Delete Store
                 </button>
             </div>
-            {invalidProduct && (
+            {invalidSucursal && (
                 <div className="flex flex-row items-center justify-center bg-white w-1/2 py-20 overflow-hidden">
                     <div className="flex flex-col" data-aos="zoom-in">
-                    <h1 className="font-bold text-2xl" data-aos="zoom-in">
+                        <h1 className="font-bold text-2xl" data-aos="zoom-in">
                             Oops, we couldn't find that product
                         </h1>
                         <p>
@@ -94,23 +96,22 @@ function DeleteProductModal(props) {
                     </div>
                 </div>
             )}
-            {!invalidProduct && producto &&
-                <div className="flex flex-row items-center justify-center bg-white w-1/2 py-20 overflow-hidden pr-20">
+            {!invalidSucursal && sucursal &&
+                <div className="flex flex-row items-center justify-center bg-white w-2/3 py-20 overflow-hidden pr-20 space-x-5">
                     <img
-                        src={`img/caps/${producto.Img}`}
-                        alt={`img/caps/${producto.Img}`}
-                        className="w-40 h-40 object-cover p-1 md:w-60 md:h-60"
+                        src={`img/stores/${sucursal.Img}`}
+                        alt={`img/stores/${sucursal.Img}`}
+                        className="w-40 h-40 object-cover p-1 md:w-60 md:h-60 rounded-lg"
                         data-aos="zoom-in"/>
                     <div className="flex flex-col" data-aos="zoom-in">
                         <h1 className="font-bold text-xl md:text-2xl lg:text-3xl">
-                            {producto.Modelo}
+                            {`${sucursal.Nombre}`}
                         </h1>
                         <p className="text-gray-600 text-md md:text-lg lg:text-xl">
-                            {producto.Categoria}
+                            {`Store number: ${sucursal.NumeroSucursal}`}
                         </p>
-                        <hr className="border-1 border-gray-300 my-1 w-full"/>
-                        <p className="font-bold text-md md:text-lg mb-2 lg:text-xl">
-                            {`$${producto.Precio}`}
+                        <p className="text-gray-600 text-md md:text-lg lg:text-lg">
+                            {`Store phone: ${sucursal.NumeroTelefono}`}
                         </p>
                     </div>
                 </div>
@@ -124,8 +125,8 @@ function DeleteProductModal(props) {
     );
 }
 
-DeleteProductModal.propTypes = {
+DeleteStoreModal.propTypes = {
     handleCloseModal: PropTypes.func.isRequired,
 };
 
-export default DeleteProductModal;
+export default DeleteStoreModal;
