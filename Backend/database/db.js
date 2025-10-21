f0fed639add6531930f5c45c7e1ba8f929db01e1
Jojashley Chaves <jojashley@hotmail.com>
2025-10-21 17:47:31 -0600

    joja: snapshot inicial sin cambios, proyecto inicial con vulnerabilidades

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize'
 
-const db = new Sequelize('IconicCaps', 'root', '', {
+const db = new Sequelize('IconicCaps', 'root', 'Jojadaya', {
   host: 'localhost',
   dialect: 'mysql',
   define: { timestamps: false }
