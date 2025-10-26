8a2978788f6087a2c42fe96632b4c35a57dd5d81
akhoz <adrian25092005@gmail.com>
2025-10-26 13:59:25 -0600

    Merge: haciendo merge desde la rama de Joja. Mi rama ahora tiene todos los cambios de Isaac y Joja (más los mios)

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -2,14 +2,17 @@ import { Sequelize } from 'sequelize'
 import dotenv from 'dotenv';
 dotenv.config();
 
+console.log(process.env.DB_NAME, process.env.DB_USER,
+)
+
 const db = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASS, 
+  process.env.DB_PASS,
   {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-  define: { timestamps: false }
-})
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    define: { timestamps: false }
+  })
 
 export default db;
